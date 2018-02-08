45a088276996150cb309e6dc58ea31a97c0251bb
Sayeed843 <sayeed.swe@gmail.com>
2018-02-08 21:25:11 +0600

    ESP8266 Wifi Module connect with Thinkspeak

--- WaterTankControl_.ino
+++ WaterTankControl_.ino
@@ -1,4 +1,21 @@
-//#include <EEPROM.h>
+
+//**********************************************************//
+/* 
+ * Project name: "Water Tank Control"
+ * Createed by "Sayeed Bin Mozahid"
+ * Daffodil International University 
+ * Department of Software Engineering
+ * Updated Feb 8, 2018
+ */
+//*********************************************************//
+
+#include <SoftwareSerial.h>    // Code to use SoftwareSerial
+#define MacAddress "M34AC75D"  // create a unique Mac Address
+SoftwareSerial esp8266 = SoftwareSerial(2,3); // Rx, Tx
+
+String apiKey = "HPJ61IC0OV07OA4J";   // Thingspeak Channel API Key
+String ssid = "TIME PASS";            // Wifi Name
+String password = "uLo$s_y0urt!me";   // Wifi Password
 
 int upperSensor = A0;
 int lowerSensor = A1;
@@ -6,21 +23,143 @@ int motorOutput = 7;
 bool motorAction = false;
 int thresholdValue = 800;
 
-//uint8_t EEPROMaddress = 130;
 
+//******************** RESPONSE TIME ****************************//
+void responseTime(int waitTime)
+{
+  long t = millis();
+  char c;
+  while(t+waitTime> millis())
+  {
+    if (esp8266.available())
+    {
+      c = esp8266.read();
+      Serial.print(c);
+    }
+  }
+}
+//********************** <RESPONSE_TIME> **************************//
+
+
+//*********************** MOTOR_VALUE *************************//
+int motorValue(boolean mStatus)
+{
+  if(mStatus)
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+//********************* <MOTOR_VALUE> ***************************//
+
+
+//********************** THING SPEAK WRITE **************************//
+boolean thingSpeakWrite(boolean motorStatus)
+{
+  String cmd ="AT+CIPSTART=\"TCP\",\"";  //TCP connection
+         cmd +="184.106.153.149";        // api.thingspeak.com
+         cmd +="\",80";
+
+  esp8266.println(cmd);       
+  Serial.println(cmd);
+  if(esp8266.find("Error"))
+  {
+    Serial.println("AT+CIPSTART Error");
+    return false; 
+  }
+
+  String getStr = "GET /update?api_key=";
+         getStr +=apiKey;
+         getStr +="&field1=";
+         getStr +=String(motorValue(motorStatus));
+         getStr +="&field2=";
+         getStr +=String(MacAddress);
+         getStr +="\r\n";
+
+  cmd = "AT+CIPSEND=";
+  cmd += String(getStr.length());
+  esp8266.println(cmd);
+  Serial.println(cmd);
+  delay(100);
+  if(esp8266.find(">"))
+  {
+    esp8266.print(getStr);
+    Serial.println(getStr);
+  }
+  else
+  {
+    esp8266.println("AT+CIPCLOSE");
+    Serial.println("AT+CIPCLOSE");
+    return false;
+  }
+  return true;
+}
+//********************* <THING_SPEAK_WRITE> ***************************//
+
+
+//********************** AGENT FUNCTION **************************//
+
+bool waterPumpControl(int upperSensorValue, int lowerSensorValue, bool motorCondition, int thresholdValue)
+{
+    if(upperSensorValue < thresholdValue and lowerSensorValue < thresholdValue)
+  {
+    digitalWrite(motorOutput,HIGH);
+    motorCondition = false;
+    Serial.println("Motor OFF condition-1");
+  }
+  else if(upperSensorValue > thresholdValue and lowerSensorValue < thresholdValue and motorCondition == true) 
+  {
+    digitalWrite(motorOutput,LOW);
+    motorCondition = true;
+    Serial.println("Motor ON condition-2"); 
+  }
+  else if(upperSensorValue > thresholdValue and lowerSensorValue > thresholdValue and motorCondition == false)
+  {
+    digitalWrite(motorOutput,LOW);
+    motorCondition = true;
+    Serial.println("Motor ON condition -3");
+  }
+  else if(upperSensorValue > thresholdValue and lowerSensorValue > thresholdValue and motorCondition == true)
+  {
+    digitalWrite(motorOutput,LOW);
+    motorCondition = true;
+    Serial.println("Motor ON condition-4");    
+  }
+  else 
+  {
+    digitalWrite(motorOutput,HIGH);
+    motorCondition = false;
+    Serial.println("Motor OFF Condition -4");  
+  }
+  delay(5000);
+  return motorCondition;
+}
+//******************** <AGENT_FUNCTION> ****************************//
+
+
+//********************* SETUP ***************************//
 void setup() {
   // put your setup code here, to run once:
-  pinMode(upperSensor,INPUT);
-  pinMode(lowerSensor,INPUT);
-  pinMode(motorOutput,OUTPUT);
+  Serial.println("Okay");
+  //debug = true;
   digitalWrite(motorOutput,HIGH);
   Serial.begin(9600);
+  esp8266.begin(9600);
+  esp8266.println("AT+CWMODE=1");   // set esp8266 wifi module as client
+  responseTime(1000);
+  esp8266.println("AT+CWJAP=\""+ssid+"\",\""+password+"\"");
+  responseTime(5000);
+  Serial.println("Setup Completed");
 }
+//********************* <SETUP> ***************************//
 
+
+//******************** LOOP ****************************//
 void loop() {
-  //motorAction = EEPROM.read(EEPROMaddress);
-  //Serial.print("Motor Action= ");
-  //Serial.print(motorAction);
+  // put your main code here, to run repeatedly:
   int upperValue = analogRead(upperSensor);
   int lowerValue = analogRead(lowerSensor);
   Serial.print("Upper Value: ");
@@ -28,50 +167,7 @@ void loop() {
   Serial.print("Lower Value: ");
   Serial.println(lowerValue);
   motorAction = waterPumpControl(upperValue,lowerValue,motorAction,thresholdValue);
-
+  thingSpeakWrite(motorAction);
+  delay(20000);
 }
-
-
-
-// Agent function
-bool waterPumpControl(int upperSensorValue, int lowerSensorValue, bool motorCondition, int thresholdValue)
-{
-    if(upperSensorValue < thresholdValue and lowerSensorValue < thresholdValue)
-  {
-    digitalWrite(motorOutput,HIGH);
-    motorCondition = false;
-   //EEPROM.write(EEPROMaddress,motorAction);
-    Serial.println("Motor OFF condition-1");
-  }
-  else if(upperSensorValue > thresholdValue and lowerSensorValue < thresholdValue and motorCondition == true) 
-  {
-    digitalWrite(motorOutput,LOW);
-    motorCondition = true;
-    //EEPROM.write(EEPROMaddress,motorAction);
-    Serial.println("Motor ON condition-2"); 
-  }
-  else if(upperSensorValue > thresholdValue and lowerSensorValue > thresholdValue and motorCondition == false)
-  {
-    digitalWrite(motorOutput,LOW);
-    motorCondition = true;
-    //EEPROM.write(EEPROMaddress,motorAction);
-    Serial.println("Motor ON condition -3");
-  }
-  else if(upperSensorValue > thresholdValue and lowerSensorValue > thresholdValue and motorCondition == true)
-  {
-    digitalWrite(motorOutput,LOW);
-    motorCondition = true;
-    //EEPROM.write(EEPROMaddress,motorAction);
-    Serial.println("Motor ON condition-4");    
-  }
-  else 
-  {
-    digitalWrite(motorOutput,HIGH);
-    motorCondition = false;
-    //EEPROM.write(EEPROMaddress,motorAction);
-    Serial.println("Motor OFF Condition -4");  
-  }
-  delay(5000);
-  return motorCondition;
-}
-
+//******************* <LOOP> *****************************//
